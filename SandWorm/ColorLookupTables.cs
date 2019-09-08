29107e8187ec26f84e28ca1fee76c443a628e349
Mariusz Hermansdorfer <mrhe@ramboll.com>
2019-09-08 20:23:02 +0200

    first doodles

--- SandWorm/ColorLookupTables.cs
+++ SandWorm/ColorLookupTables.cs
@@ -4,11 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using Rhino.Display;
 
 namespace SandWorm
 {
     public static class ColorLookupTables
     {
-        Color[] = Color
+        
+
+        public static Color[] ComputeLookupTable(int waterLevel, Color[] lookupTable)
+        {
+            //precompute all vertex colors
+            int j = 0;
+            for (int i = waterLevel; i < lookupTable.Length; i++) //below water level
+            {
+                lookupTable[i] = new ColorHSL(0.6, 0.6, 0.60 - (j * 0.02)).ToArgbColor();
+                j++;
+            }
+
+            j = 0;
+            for (int i = waterLevel; i > 0; i--) //above water level
+            {
+                lookupTable[i] = new ColorHSL(0.01 + (j * 0.01), 1.0, 0.5).ToArgbColor();
+                j++;
+            }
+            return lookupTable;
+        }
+
+        //Color.FromArgb(128, 128, 128);
     }
 }
