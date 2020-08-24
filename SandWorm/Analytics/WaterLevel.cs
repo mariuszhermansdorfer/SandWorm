b4cddad5bf8af10665bd73dbc5cb37babc722555
BarusXXX <kane@kaneborg.com>
2020-08-24 09:52:20 +0300

    Re-orient waterplane
    Water Plane Orientation for setup

--- SandWorm/Analytics/WaterLevel.cs
+++ SandWorm/Analytics/WaterLevel.cs
@@ -15,7 +15,7 @@ namespace SandWorm.Analytics
             var waterPlane = new Plane(new Point3d(bounds.Max.X, bounds.Max.Y, waterLevel), new Vector3d(0, 0, 1));
             var waterSrf = new PlaneSurface(waterPlane,
                 new Interval(bounds.Min.X, bounds.Max.X),
-                new Interval(bounds.Min.Y, bounds.Max.Y)
+                new Interval(-bounds.Max.Y, -bounds.Min.Y) //Changed to -Y to match frame reversal.
             );
             outputGeometry.Add(waterSrf);
         }
